a4331f4688431cc015f82e2260452077b10d2e0f
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-11-22 22:16:06 +0100

    Update MockAuth.ts

--- src/app/MockServices/MockAuth.ts
+++ src/app/MockServices/MockAuth.ts
@@ -1,33 +1,34 @@
+import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
 import { User } from "../models/User.model";
 import { UserPrivacySetting } from "../models/UserPrivacySetting.model";
 import { UserProfile } from "../models/UserProfile.model";
 
 export class MockAuthenticationService {
-    SIDDHARTH_USER_PROFILE = new UserProfile(
-      "Siddharth Singh",
-      "https://avatars.githubusercontent.com/u/68241942?v=4",
-      "https://img5.goodfon.com/original/1366x768/6/89/dresden-drezden-germaniia.jpg",
-      "Munich, Germany",
-      new Date(),
-      "Founder of Glitter",
-      new Date(),
-      "Male",
-      "Single",
-      "Yawn !",
-      true,
-      true,
-      [],
-      []
-    );
-  
-    SIDDHARTH_USER_PRIVACY = new UserPrivacySetting(
-      true,
-      true
-    );
-  
+  logged_in = new BehaviorSubject<boolean>(false);
+  SIDDHARTH_USER_PROFILE = new UserProfile(
+    "Siddharth Singh",
+    "https://avatars.githubusercontent.com/u/68241942?v=4",
+    "https://img5.goodfon.com/original/1366x768/6/89/dresden-drezden-germaniia.jpg",
+    "Munich, Germany",
+    new Date(),
+    "Founder of Glitter",
+    new Date(),
+    "Male",
+    "Single",
+    "Yawn !",
+    true,
+    true,
+    [],
+    []
+  );
 
-    get_current_user(): User{
-      return new User("siddharth", this.SIDDHARTH_USER_PROFILE, this.SIDDHARTH_USER_PRIVACY);
-    }
+  SIDDHARTH_USER_PRIVACY = new UserPrivacySetting(
+    true,
+    true
+  );
+
+
+  get_current_user(): User {
+    return new User("siddharth", this.SIDDHARTH_USER_PROFILE, this.SIDDHARTH_USER_PRIVACY);
   }
-  
+}
