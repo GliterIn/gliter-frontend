47d829deb71bab37e440a20079f7d39d29d23129
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-11-22 06:43:43 +0100

    Update utils.service.spec.ts

--- src/app/service/utils.service.spec.ts
+++ src/app/service/utils.service.spec.ts
@@ -14,6 +14,6 @@ describe('UtilsService', () => {
     expect(service).toBeTruthy();
   });
   it('should parse timestamp short', ()=>{
-    expect(service.parseTimestampShort(new Date(1669095632000))).toEqual('23 Nov 2022');
+    expect(service.parseTimestampShort(new Date(1669095632000))).toEqual('22 Nov 2022');
   });
 });
