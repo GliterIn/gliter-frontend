5b1e3f84460502b8d88b4dded6caadaea0bff6ad
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-01-03 22:57:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJ6F579VTmAXLK3OzYsvjddBZA6peHMA8",
+    authDomain: "gliter-qa.firebaseapp.com",
+    projectId: "gliter-qa",
+    storageBucket: "gliter-qa.appspot.com",
+    messagingSenderId: "1031641354292",
+    appId: "1:1031641354292:web:e9db2f6132dbca3d4a3a61",
+    measurementId: "G-W3671JGJ9F"
+  }
 };
