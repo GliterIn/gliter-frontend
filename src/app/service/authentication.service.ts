868d90606547e0f81e37d264d1a3a0d5bf73f5d6
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-01-21 23:32:32 +0100

    Update authentication.service.ts

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -10,8 +10,8 @@ import { UserCredential } from 'firebase/auth';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  API_BASE_URL = 'http://localhost:8000/api';
-  //API_BASE_URL = 'https://gliter-backend.herokuapp.com/api';
+  //API_BASE_URL = 'http://localhost:8000/api';
+  API_BASE_URL = 'https://gliter-backend.herokuapp.com/api';
   logged_in_user = new BehaviorSubject<UserProfile | null>(null);
   user_token = new BehaviorSubject<string>("");
 
