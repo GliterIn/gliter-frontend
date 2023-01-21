ae8a7827fb422d8115e19d2e04d2505e7ca67891
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-01-21 22:23:05 +0100

    Update edit-profile.component.spec.ts

--- src/app/components/edit-profile/edit-profile.component.spec.ts
+++ src/app/components/edit-profile/edit-profile.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockAuthenticationService } from 'src/app/MockServices/MockAuth';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 import { EditProfileComponent } from './edit-profile.component';
 
@@ -8,7 +10,11 @@ describe('EditProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditProfileComponent ]
+      declarations: [ EditProfileComponent ],
+      providers: [{
+        provide: AuthenticationService,
+        useClass: MockAuthenticationService
+      }]
     })
     .compileComponents();
 
