c39d4f009d2b95bc0a517205780cf2990c6f8cd4
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-01-28 23:41:15 +0100

    Fixed TImestamp

--- src/app/service/utils.service.ts
+++ src/app/service/utils.service.ts
@@ -16,6 +16,21 @@ export class UtilsService {
   }
   parseTimestamp(date: Date) {
     var parsedDate = new Date(date);
-    return parsedDate.getUTCDate() + " " + this.MONTHS[parsedDate.getUTCMonth()] + ". " + parsedDate.getUTCFullYear() + ", " + parsedDate.getUTCHours() + ":" + parsedDate.getUTCMinutes();
+    var am_pm = 'AM';
+    var time_hr_value = parsedDate.getUTCHours();
+    var time_hr = parsedDate.getUTCHours().toString();
+    var time_min = parsedDate.getUTCMinutes().toString();
+    if(parsedDate.getUTCHours() >= 12){
+      am_pm = 'PM';
+      time_hr_value = time_hr_value-12;
+    }
+    if(time_hr_value<=9){
+      time_hr = "0"+time_hr_value.toString();
+    }
+    
+    if(parsedDate.getUTCMinutes()<=9){
+      time_min = "0"+parsedDate.getUTCMinutes().toString();
+    }
+    return parsedDate.getUTCDate() + " " + this.MONTHS[parsedDate.getUTCMonth()] + ". " + parsedDate.getUTCFullYear() + ", " + time_hr + ":" + time_min + " " + am_pm;
   }
 }
