25b8dfda8d9c39968e272457a8744bf3502b15f7
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-01-28 16:23:47 +0100

    Fixed Tests

--- src/app/components/profile-page/profile-page.component.spec.ts
+++ src/app/components/profile-page/profile-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { MockActivatedRoute } from 'src/app/MockServices/MockActivatedRoute';
 import { MockAuthenticationService } from 'src/app/MockServices/MockAuth';
+import { MockDatabaseService } from 'src/app/MockServices/MockDatabase';
 import { AuthenticationService } from 'src/app/service/authentication.service';
+import { DatabaseService } from 'src/app/service/database.service';
 
 import { ProfilePageComponent } from './profile-page.component';
 
@@ -15,7 +19,15 @@ describe('ProfilePageComponent', () => {
         {
           provide: AuthenticationService,
           useClass: MockAuthenticationService
-        }
+        },
+        {
+          provide: DatabaseService,
+          useClass: MockDatabaseService
+        },
+        {
+          provide: ActivatedRoute,
+          useClass: MockActivatedRoute
+        },
       ]
     })
     .compileComponents();
