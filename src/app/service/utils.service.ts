96fe5524766860101483ffb15901a71d82eb279c
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2023-02-19 11:32:15 +0100

    Update utils.service.ts

--- src/app/service/utils.service.ts
+++ src/app/service/utils.service.ts
@@ -1,36 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import {formatDate} from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
-  MONTHS = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
-  ];
   constructor() { }
 
   parseTimestampShort(date: Date) {
-    var parsedDate = new Date(date);
-    return parsedDate.getUTCDate() + " " + this.MONTHS[parsedDate.getUTCMonth()] + " " + parsedDate.getUTCFullYear();
+    return formatDate(date,'d MMM yyyy','en-US');
   }
   parseTimestamp(date: Date) {
-    var parsedDate = new Date(date);
-    var am_pm = 'AM';
-    var time_hr_value = parsedDate.getUTCHours();
-    var time_hr = parsedDate.getUTCHours().toString();
-    var time_min = parsedDate.getUTCMinutes().toString();
-    if(parsedDate.getUTCHours() >= 12){
-      am_pm = 'PM';
-      time_hr_value = time_hr_value-12;
-    }
-    if(time_hr_value<=9){
-      time_hr = "0"+time_hr_value.toString();
-    }
-    
-    if(parsedDate.getUTCMinutes()<=9){
-      time_min = "0"+parsedDate.getUTCMinutes().toString();
-    }
-    return parsedDate.getUTCDate() + " " + this.MONTHS[parsedDate.getUTCMonth()] + ". " + parsedDate.getUTCFullYear() + ", " + time_hr + ":" + time_min + " " + am_pm;
+    return formatDate(date,'dd MMM. yyyy, hh:mm a','en-US');
   }
 }
